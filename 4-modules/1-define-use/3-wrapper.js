2d5260f60485a2efc7699f634725d101e61f0381
Bob Murray <murray395@gmail.com>
2021-04-22 16:21:52 -0400

    and even more

--- 4-modules/1-define-use/3-wrapper.js
+++ 4-modules/1-define-use/3-wrapper.js
@@ -1,1 +1,8 @@
+//function (exports, module, require, __filename, __dirname){
+let g = 5;
 console.log(arguments);
+exports.a = 42;
+module.exports.b = 74;
+
+// return module.exports;
+//}(module.exports, module)
